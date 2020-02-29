5517329bad1dde0cd9102c39805fea4608b91ca2
objectorange <objectorange@1m5.io>
2020-02-29 03:49:33 -0700

    begin adding data structs

--- platform/src/lib.rs
+++ platform/src/lib.rs
@@ -1,6 +1,48 @@
 mod data {
+    use std::collections::VecDeque;
+
+    pub struct DynamicRoutingSlip {
+        routes: VecDeque<Route>,
+        in_progress: bool
+    }
+
+    impl DynamicRoutingSlip {
+        pub fn start(&self) {
+
+        }
+        pub fn next_route(&mut self) {
+
+        }
+        pub fn peek_at_next_route(&self) {
+
+        }
+        pub fn number_remaining_routes(&self) {
+
+        }
+    }
+
     pub struct Envelope {
-        pub id:
+        pub id: i64,
+        pub client: i64,
+        pub reply_to_client: bool,
+        pub client_reply_action: String,
+        pub external: bool,
+        pub routing_slip: DynamicRoutingSlip,
+        pub man_con: ManCon,
+        pub min_delay: i64,
+        pub max_delay: i64
+    }
+
+    pub enum ManCon {
+        NEO, EXTREME, VERYHIGH, HIGH, MEDIUM, LOW, NONE, UNKNOWN
+    }
+
+    pub struct Route {
+        pub id: i64,
+        pub service: String,
+        pub op: String,
+        pub routed: bool
+
     }
 }
 
