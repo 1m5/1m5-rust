f991ea6dedcae9436c0504168cd1615ca0df1431
objectorange <objectorange@1m5.io>
2020-03-03 21:56:17 -0700

    opt implemented...testing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,40 @@
+use log::{info};
+use std::path::PathBuf;
+use structopt::StructOpt;
 use platform;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, StructOpt)]
+#[structopt(name = "1M5 CLI", about = "Exercise the 1M5 Platform")]
+struct Opt {
+    /// Activate debug mode
+    // short and long flags (-d, --debug) will be deduced from the field's name
+    #[structopt(short, long)]
+    debug: bool,
+
+    /// Set speed
+    // we don't want to name it "speed", need to look smart
+    #[structopt(short = "v", long = "velocity", default_value = "42")]
+    speed: f64,
+
+    /// Where to write the output: to `stdout` or `file`
+    #[structopt(short)]
+    out_type: String,
+
+    /// File name: only required when `out` is set to `file`
+    #[structopt(name = "FILE", required_if("out_type", "file"))]
+    file_name: String,
+
+    /// Input file
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+
+    /// Output file, stdout if not present
+    #[structopt(parse(from_os_str))]
+    output: Option<PathBuf>,
 }
+
+fn main() {
+    let opt = Opt::from_args();
+    info!("{:?}", opt);
+}
+
